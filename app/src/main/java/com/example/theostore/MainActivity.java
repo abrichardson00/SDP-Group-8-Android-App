1b7c6d4bb870080fb10b3f37ddeacb2b697c2be2
lydialamont <s1846854@ed.ac.uk>
2021-02-15 20:01:33 +0000

    code

---
+++ app/src/main/java/com/example/theostore/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.theostore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent (MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
